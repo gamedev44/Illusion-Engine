31214689f469bc404889e719498cffbcae7c5799
gamedev44 <108845184+gamedev44@users.noreply.github.com>
2024-02-01 15:29:07 -0600

    PUSHED HOTFIX ONLY A FEW MORE ERRORS TO GO AND WE CAN FINISH THIS THING!
    1. updated- working
    HOTFIX 1 GOT RID OF and fixed over 239 ERRORS
    ---------------------------------------------------------------
    2.
     AND then i made a simple ingore command to hide
    
     9 OTHERS RELATED MOSTLY TO To
    a file named CommonHeaders.h in relation to a file named ToolsCommon.h i needs pointers to point to them right as well as ContentTools.vcxproj needs to be fixed

--- Primal-master/Engine/Components/ComponentsCommon.h
+++
@@ -1,5 +0,0 @@
-// Copyright (c) Arash Khatami
-// Distributed under the MIT license. See the LICENSE file in the project root for more information.
-#pragma once
-#include "../Common/CommonHeaders.h"
-#include "../EngineAPI/GameEntity.h"
