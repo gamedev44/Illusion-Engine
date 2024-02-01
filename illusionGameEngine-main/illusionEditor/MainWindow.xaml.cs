897d5618b940d72df88e159cea83e1c906cf6cbd
Hash_Buoy <harshad.asawale@gmail.com>
2024-02-01 09:30:47 +0530

    Renamed engine to illusion

---
+++ illusionGameEngine-main/illusionEditor/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using illusionEditor.GameProject;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace illusionEditor
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Loaded += OnMainWindowLoaded;
+            Closing += OnMainWindowClosing;
+        }
+
+        private void OnMainWindowLoaded(object sender, RoutedEventArgs e)
+        {
+            Loaded -= OnMainWindowLoaded;
+            OpenProjectBrowserDialog();
+        }
+
+        private void OnMainWindowClosing(object sender, CancelEventArgs e)
+        {
+            Closing -= OnMainWindowClosing;
+            Project.Current?.Unload();
+        }
+
+        private void OpenProjectBrowserDialog()
+        {
+            var projectBrowser = new ProjBrowserDialog();
+            if (projectBrowser.ShowDialog() == false || projectBrowser.DataContext == null)
+            {
+                Application.Current.Shutdown();
+            }
+            else
+            {
+                Project.Current?.Unload();
+                DataContext = projectBrowser.DataContext;
+            }
+            
+        }
+    }
+}
